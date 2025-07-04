e52f045b8a95bf317bde6b25b04d9f256f061056
Wilson Aballey <willywallet001@gmail.com>
2025-07-04 09:29:52 +0000

    solution

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,18 @@
-let js = "Wilson is FUN";
-        if (js === "Wilson is FUN") {
-            alert('Hello World');
-        }
 
 
-console.log(40 + 8 - 40);
+let markMass = 78;
+let markHeight = 1.69;
+let johnMass = 92;
+let johnHeight = 1.95;
+
+
+calMarkBmi = markMass/markHeight **2;
+calJohnBmi = johnMass/(johnHeight**2);
+let markHigherBMI = calMarkBmi>calJohnBmi;
+
+console.log(calJohnBmi,calMarkBmi,markHigherBMI);
+
+
+
+
+
