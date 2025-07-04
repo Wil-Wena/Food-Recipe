10761ae04faff4710a92298d856613dd5b20a5de
Wilson Aballey <willywallet001@gmail.com>
2025-07-04 10:21:02 +0000

    literals

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,21 @@
 
 
-let markMass = 78;
+/* let markMass = 78;
 let markHeight = 1.69;
 let johnMass = 92;
 let johnHeight = 1.95;
 
 
-calMarkBmi = markMass/markHeight **2;
-calJohnBmi = johnMass/(johnHeight**2);
+const calMarkBmi = markMass/markHeight **2;
+const calJohnBmi = johnMass/(johnHeight**2);
 let markHigherBMI = calMarkBmi>calJohnBmi;
 
-console.log(calJohnBmi,calMarkBmi,markHigherBMI);
+console.log(calJohnBmi,calMarkBmi,markHigherBMI); */
 
+//Template Literals
+/* const ageWilson =14
+const firstName = 'Wilson'
 
+const printDetails = `I'm ${firstName} and I'm ${ageWilson} years of age.`
 
-
-
+console.log(printDetails) */
