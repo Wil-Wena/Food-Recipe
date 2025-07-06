5c18728041b5c9e4d042014079cd5de2dbc2eec6
Wilson Aballey <willywallet001@gmail.com>
2025-07-06 16:05:48 +0000

    Practice

--- javascript/script.js
+++ javascript/script.js
@@ -272,25 +272,38 @@ while (dice !==6) {
     dice = Math.trunc(Math.random() * 6) + 1;
 } */
 
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-};
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
-for (let i = 0; i < bills.length; i++) {
-  let m = calcTip(bills[i]);
-  tips.push(m);
-  totals.push(m + bills[i]);
-}
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
+// for (let i = 0; i < bills.length; i++) {
+//   let m = calcTip(bills[i]);
+//   tips.push(m);
+//   totals.push(m + bills[i]);
+// }
 
-console.log(tips, totals);
+// console.log(tips, totals);
 
-function calcAverage(arr) {
-  let sum = 0;
+// function calcAverage(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum = sum + arr[i];
+//   }
+//   return sum / arr.length;
+// }
+// console.log(calcAverage(totals));
+// console.log();
+const testData1 = [17, 21, 23];
+const testData2 = [12, 5, -5, 0, 4];
+const printForecast = function (arr) {
+  let str = "";
   for (let i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
+    const m = `...${arr[i]}C in ${i + 1} days`;
+    str += m;
   }
-  return sum / arr.length;
-}
-console.log(calcAverage(totals));
+  console.log(str);
+};
+printForecast(testData1);
+printForecast(testData2);
